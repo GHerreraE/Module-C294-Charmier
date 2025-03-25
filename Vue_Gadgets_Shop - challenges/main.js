4151d9102f02a5e91cf342e678ae9e9c8de87969
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-03-25 15:59:27 +0100

    Challenge 4 - Done

--- Vue_Gadgets_Shop - challenges/main.js
+++ Vue_Gadgets_Shop - challenges/main.js
@@ -4,10 +4,29 @@ const app = Vue.createApp({
       title: "Vue Gadget Shop",
       description:
         "Découvrez les derniers gadgets électroniques de haute technologie !",
-      nom: "Smartphone XZ",
-      prix: 799,
-      image: "./assets/phone.jpg",
-      inStock: true,
+      gadgets: [
+        {
+          id: 1,
+          nom: "Smartphone XZ",
+          prix: 799,
+          image: "./assets/phone.jpg",
+          inStock: true,
+        },
+        {
+          id: 2,
+          nom: "Laptop Pro",
+          prix: 1299,
+          image: "./assets/laptop.jpg",
+          inStock: false,
+        },
+        {
+          id: 3,
+          nom: "Ecouteurs Bluetooth",
+          prix: 199,
+          image: "./assets/earbuds.jpg",
+          inStock: true,
+        },
+      ],
     };
   },
 });
